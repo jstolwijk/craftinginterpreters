a11fdeb09c82216bb5dcaf96e9fb0d177396a3f3
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-04-13 17:07:38 +0200

    Simplify code

--- jlox/src/grammar_generator/PrintAst.kt
+++ jlox/src/grammar_generator/PrintAst.kt
@@ -2,10 +2,13 @@ package grammar_generator
 
 import grammar_generator.Expression.*
 import grammar_generator.Expression.Literal.*
+import lox.Token
+import lox.TokenType
+import java.math.BigDecimal
 
 fun Expression.toPrettyString(): String {
     return when(this) {
-        is Binary -> parenthesize(operator.lexeme, left, right)
+        is Binary -> "(${operator.lexeme} ${left.toPrettyString()} ${right.toPrettyString()})"
         is Literal -> when(this) {
             is LiteralNumber -> value.stripTrailingZeros().toString()
             is LiteralString -> value
@@ -13,27 +16,37 @@ fun Expression.toPrettyString(): String {
             is False -> "false"
             is Nil -> "nil"
         }
-        is Grouping ->  parenthesize("group", expression)
-        is Unary -> parenthesize(operator.lexeme, right)
+        is Grouping ->  "(group ${expression.toPrettyString()})"
+        is Unary -> "(${operator.lexeme} ${right.toPrettyString()})"
     }
 }
 
-private fun parenthesize(name: String, vararg expressions: Expression): String {
-    val items = listOf(name) + expressions.map { it.toPrettyString() }
-    return items.joinToString(prefix = "(", postfix = ")", separator = " ")
+fun Expression.toReversePolishNotation(): String {
+    return when(this) {
+        is Binary -> "${left.toReversePolishNotation()} ${right.toReversePolishNotation()} ${operator.lexeme}"
+        is Literal -> when(this) {
+            is LiteralNumber -> value.stripTrailingZeros().toString()
+            is LiteralString -> value
+            is True -> "true"
+            is False -> "false"
+            is Nil -> "nil"
+        }
+        is Grouping -> expression.toReversePolishNotation()
+        is Unary -> "${right.toReversePolishNotation()} ${operator.lexeme}"
+    }
 }
 
 // TEST FUNCTION
-//fun main() {
-//    println(
-//        Expression.Binary(
-//            left = Expression.Unary(
-//                operator = Token(TokenType.MINUS, "-", null, 1),
-//                right = Expression.Literal.LiteralNumber(BigDecimal("123.00"))
-//            ),
-//            operator = Token(TokenType.STAR, "*", null, 1),
-//            right = Expression.Grouping(Expression.Literal.LiteralNumber(BigDecimal("45.67")))
-//        ).toPrettyString()
-//    )
-//}
+fun main() {
+    println(
+        Binary(
+            left = Unary(
+                operator = Token(TokenType.MINUS, "-", null, 1),
+                right = LiteralNumber(BigDecimal("123.00"))
+            ),
+            operator = Token(TokenType.STAR, "*", null, 1),
+            right = Grouping(LiteralNumber(BigDecimal("45.67")))
+        ).toPrettyString()
+    )
+}
 
